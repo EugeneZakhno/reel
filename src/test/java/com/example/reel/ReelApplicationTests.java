84bdfa2d38041b90e57e363b0ee7a3979a41d7d3
EugeneZakhno <zohan1234567@gmail.com>
2020-08-20 16:11:22 +0300

    first commit

---
+++ src/test/java/com/example/reel/ReelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
